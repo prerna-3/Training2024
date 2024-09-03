25e77111e9b4bdfe277c36423f607686e78605fb
prerna-3 <prernasharma2k78@gmial.com>
2024-09-03 11:31:30 +0000

    updating

---
+++ codes/oddeven.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int number;  
+
+	    
+		    printf("Enter an integer: ");
+			    scanf("%d", &number);
+
+				    
+					    if (number % 2 == 0) {
+						        printf("%d is an Even number.\n", number);
+								    } else {
+									        printf("%d is an Odd number.\n", number);
+											    }
+
+												    return 0;
+													}
+
